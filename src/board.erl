6d83680a67ca624bbd4fc6bd69705d44fa4c1c68
James Greenhalgh <james.greenhalgh@gmail.com>
2016-05-04 08:59:53 +1000

    Checking for blank squares along move-path

--- src/board.erl
+++ src/board.erl
@@ -22,18 +22,84 @@ test() ->
 
 is_valid_move(Board, From, To) ->
 	% is from square empty?
-	{_Type, _Colour} = get_square(From, Board),
+	{Type, Colour} = get_square(From, Board),
 	% is destination empty, or contain enemy piece?
-	destination_possible(Colour, To),
+	is_empty_or_enemy(Board, Colour, To) andalso
 	% is the to location potentially legal??
-	legal_move(Type, From, To),
-	is_blocked(From, To),
-	no_check,
-	
-	true.
+	legal_move(Type, From, To) andalso
+	is_not_blocked(Board, From, To).
 
-destination_possible(Colour, To) ->
-	get_square(To).
+
+is_not_blocked(Board, From, To) ->
+	case get_square(From, Board) of
+		{knight,_} -> true;
+		{king,_} -> true;
+		%% What about first move??
+		{pawn,_} -> true;
+		{rook,_} -> 
+			squares_are_empty(get_intermediate_squares(From, To));
+		{bishop,_} -> 
+			squares_are_empty(get_intermediate_squares(From, To));
+		{queen,_} -> 
+			squares_are_empty(get_intermediate_squares(From, To))
+	end.
+	% knight doesn't need to check
+	% king and pawn already checked
+	
+	% need to get all squares between diagonals
+	% need to get all squares between straights
+
+squares_are_empty(_ListOfSquares) -> true.
+
+% Horizontal/vertical
+get_intermediate_squares({A,B}, {A,Y}) when B<Y ->
+	[{A,Z} || Z <- lists:seq(B,Y)];
+get_intermediate_squares({A,B}, {A,Y}) ->
+	[{A,Z} || Z <- lists:reverse(lists:seq(B,Y))];
+get_intermediate_squares({A,B}, {X,B}) when A<X ->
+	[{Z,B} || Z <- lists:seq(A,X)];
+get_intermediate_squares({A,B}, {X,B}) ->
+	[{Z,B} || Z <- lists:reverse(lists:seq(A,X))];
+% Diagonal
+get_intermediate_squares({A,B}, {X,Y}) when abs(X-A) =:= abs(B-Y) ->
+%	case {abs(X-A), abs(B-Y)} of
+%		{C,C} ->
+			FileList = case A<X of
+				true ->
+					lists:seq(A,X);
+				false ->
+					lists:reverse(lists:seq(X,A))
+			end,
+			RankList = case B<Y of
+				true ->
+					lists:seq(B,Y);
+				false ->
+					lists:reverse(lists:seq(Y,B))
+			end,
+%	end,
+	lists:zip(FileList, RankList).
+	
+legal_move(Type, From, To) ->
+	case Type of
+		knight -> is_horsie(From, To);
+		queen -> is_straight(From, To) orelse
+				is_diagonal(From, To);
+		bishop -> is_diagonal(From, To);
+		rook -> is_straight(From, To);
+		pawn -> is_straight(From, To) orelse 
+			is_diagonal(From, To);
+		king -> is_straight(From,To) orelse
+			is_diagonal(From, To)
+	end.
+
+is_empty_or_enemy(Board, Colour, To) ->
+	%Also checks if From=To
+	DestPiece = get_square(Board, To),
+	case DestPiece of
+		{} -> true;
+		{_, Colour} -> false;
+		{_, _} -> true
+	end.
 
 is_diagonal({A,B},{X,Y}) ->
 	case {abs(file_to_integer(X) - file_to_integer(A)), abs(Y-B)} of
